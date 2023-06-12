5a9315f3fceab1ad2e2e2553fd5505389e6d0d4d
Stefano Devoti <stefano.devoti92@gmail.com>
2023-06-12 16:05:26 +0200

    Created IngredientController CRUD

---
+++ src/main/java/com/example/ceresto/eat/controller/IngredientController.java
@@ -0,0 +1,49 @@
+package com.example.ceresto.eat.controller;
+
+import com.example.ceresto.eat.model.Customer;
+import com.example.ceresto.eat.model.Ingredient;
+import com.example.ceresto.eat.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ingredients")
+public class IngredientController {
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @PostMapping("/create")
+    public void create(@RequestBody Ingredient ingredient) {
+        ingredientRepository.saveAndFlush(ingredient);
+    }
+
+    @GetMapping("/get-all")
+    public List<Ingredient> getAll() {
+        return ingredientRepository.findAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public Optional<Ingredient> getById(@PathVariable Long id) {
+        return ingredientRepository.findById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public String update(@PathVariable Long id, @RequestBody Ingredient ingredient){
+        ingredientRepository.deleteById(id);
+        ingredientRepository.save(ingredient);
+        return "Ingredient updated";
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        ingredientRepository.deleteById(id);
+    }
+
+    @DeleteMapping("deleteAll")
+    public void deleteAll() {
+        ingredientRepository.deleteAll();
+    }
+}
